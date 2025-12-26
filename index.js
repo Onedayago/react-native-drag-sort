9178e625bbf0d9885d01c619cbbb9fdbeeb140ee
liushun <1457048060@qq.com>
2025-12-26 14:21:59 +0800

    增加 npm 包发布

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
 /**
  * @file index.js
- * @description 
+ * @description Main entry point for react-native-drag-sort
  * @author liushun
- * @created 2025/12/24 14:28
- * @lastModified 2025/12/24 14:28
  */
 
-
 import DragSortView from "./lib/DragSortView";
-import TestMoreDragSort from "./TestMoreDragSort";
-import TestDragSort from "./TestDragSort";
 
-
-export {
-	DragSortView,
-	TestMoreDragSort,
-	TestDragSort,
-}
+export default DragSortView;
+export { DragSortView };
